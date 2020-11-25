11922fa17a282f9e483c653181c9a7b395593bd3
Saleh Alkhalifa <salehesam@gmail.com>
2020-11-25 15:15:52 -0500

    Fixed CORS

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+               'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+               'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    next();
+});
+
 // const mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost/whiteboard', {useNewUrlParser: true});
 //
