15640367d7aba6d1ba7a0885e71e36e0646b059e
Saleh Alkhalifa <salehesam@gmail.com>
2020-12-01 13:46:30 -0500

    New CORS

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers',
+//                'Content-Type, X-Requested-With, Origin');
+//     res.header('Access-Control-Allow-Methods',
+//                'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+//     next();
+// });
+
+
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers',
@@ -14,6 +24,8 @@ app.use(function (req, res, next) {
                'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     next();
 });
+
+
 
 // const mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost/whiteboard', {useNewUrlParser: true});
@@ -33,6 +45,6 @@ require("./controllers/snippets-controller")(app)
 
 // app.listen(3000)
 
-app.listen(process.env.PORT || 3000, () =>
+app.listen(process.env.PORT || 3003, () =>
     console.log("Server is running...")
 );
