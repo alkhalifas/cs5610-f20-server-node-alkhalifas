9cee266914545ea539afbe4622facae69e010353
Saleh Alkhalifa <salehesam@gmail.com>
2020-12-06 22:51:31 -0500

    Changing CORS policy

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// app.use(function (req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers',
-//                'Content-Type, X-Requested-With, Origin');
-//     res.header('Access-Control-Allow-Methods',
-//                'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-//     next();
-// });
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+               'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+               'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    next();
+});
 
 app.use(
     cors({
